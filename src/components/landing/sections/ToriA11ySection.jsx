6453c9eef168abf164c06de8d84254887877f802
adirboko17 <bokobzadir@gmail.com>
2026-09-16 22:19:47 +0300

    ..

--- src/components/landing/sections/ToriA11ySection.jsx
+++ src/components/landing/sections/ToriA11ySection.jsx
@@ -5,7 +5,7 @@ export default function ToriA11ySection() {
       style={{
         position: "fixed",
         zIndex: "130",
-        bottom: "32px",
+        bottom: "24px",
         right: "24px",
         display: "grid",
         justifyItems: "right",
